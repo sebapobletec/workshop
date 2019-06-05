65637e0a5ae4e2f565434e1ce8249377c8b85776
Sebastian Poblete <sebapobletec@gmail.com>
2019-06-05 00:39:11 -0400

    Add attendance model and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_040626) do
+ActiveRecord::Schema.define(version: 2019_06_05_043547) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.boolean "attend", default: false
+    t.integer "event_id"
+    t.integer "worker_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_attendances_on_event_id"
+    t.index ["worker_id"], name: "index_attendances_on_worker_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
