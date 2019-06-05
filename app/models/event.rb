8bdc0dad6f59f91a020ca23fc8b002d428e692ee
Sebastian Poblete <sebapobletec@gmail.com>
2019-06-05 00:46:57 -0400

    edit models to n-n

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :company
+  has_many :attendances
+  has_many :workers, through: :attendances
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,3 +1,5 @@
 class Worker < ApplicationRecord
   belongs_to :company
+  has_many :attendances
+  has_many :events, through: :attendances
 end
