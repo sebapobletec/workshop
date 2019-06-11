05a951d3531127ce5e8b27851950e03a6e61f968
Sebastian Poblete <sebapobletec@gmail.com>
2019-06-10 21:59:27 -0400

    Validate presence

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,4 +3,7 @@ class Company < ApplicationRecord
   has_many :workers, :dependent => :destroy
   has_many :events, :dependent => :destroy
   validates_uniqueness_of :name
+  validates_presence_of :name
+  validates_presence_of :rut
+  validates_presence_of :adress 
 end
